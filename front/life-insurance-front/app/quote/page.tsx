52097f704d162d1be58b50ff3ee9bd005aa247c7
justin-zngly <justinike.cunningham@outlook.com>
2024-02-13 16:32:08 +0000

    Initial animation

--- front/life-insurance-front/app/quote/page.tsx
+++ front/life-insurance-front/app/quote/page.tsx
@@ -5,27 +5,106 @@ import { useState } from "react";
 import QuoteQuestion from "@/components/QuoteQuestion/QuoteQuestion";
 import SingleLineTextBox from "@/components/SingleLineTextBox";
 
+const QuoteBlock = ({
+  position,
+  current,
+  children,
+}: {
+  position: number;
+  current: number;
+  children: React.ReactNode;
+}) => {
+  const st = "translateX(calc(" + (current - position) + "*30px))";
+  const style = {
+    transform:
+      "translate(" +
+      (position - current) * 200 +
+      "px) scale(" +
+      (current !== position ? "0.8" : "1") +
+      ")",
+  };
+  return (
+    <div
+      style={style}
+      className="flex justify-center items-center h-full transition duration-300"
+    >
+      {children}
+    </div>
+  );
+};
+
 const Quote = () => {
   const [name, setName] = useState<string>("");
+  const [curr, setCurr] = useState<number>(0);
+  const [t, setT] = useState<boolean>(false);
+
+  const edgeStyle = "flex justify-center items-center h-full w-1/4";
 
   return (
-    <QuoteQuestion
-      question="Please enter some preliminary information"
-      components={[
-        <SingleLineTextBox
-          label="Name"
-          placeholder="John Doe"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />,
-        <SingleLineTextBox
-          label="Address"
-          placeholder="123 Main Street, Town"
-          type="text"
-        />,
-      ]}
-    />
+    <div className="flex flex-row justify-center items-center h-screen">
+      <button className="z-10" onClick={() => setCurr((curr) => curr + 1)}>
+        Test
+      </button>
+      <div className={`flex flex-row grow justify-between items-center h-full`}>
+        <QuoteBlock position={0} current={curr}>
+          <QuoteQuestion
+            question="Please enter some preliminary information"
+            active={t}
+            components={[
+              <SingleLineTextBox
+                label="Name"
+                placeholder="John Doe"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />,
+              <SingleLineTextBox
+                label="Address"
+                placeholder="123 Main Street, Town"
+                type="text"
+              />,
+            ]}
+          />
+        </QuoteBlock>
+
+        <QuoteQuestion
+          question="Please enter some preliminary information"
+          active
+          components={[
+            <SingleLineTextBox
+              label="Name"
+              placeholder="John Doe"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />,
+            <SingleLineTextBox
+              label="Address"
+              placeholder="123 Main Street, Town"
+              type="text"
+            />,
+          ]}
+        />
+
+        <QuoteQuestion
+          question="Please enter some preliminary information"
+          components={[
+            <SingleLineTextBox
+              label="Name"
+              placeholder="John Doe"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />,
+            <SingleLineTextBox
+              label="Address"
+              placeholder="123 Main Street, Town"
+              type="text"
+            />,
+          ]}
+        />
+      </div>
+    </div>
   );
 };
 
--- front/life-insurance-front/components/QuoteQuestion/QuoteQuestion.tsx
+++ front/life-insurance-front/components/QuoteQuestion/QuoteQuestion.tsx
@@ -4,29 +4,32 @@ import SingleLineTextBox from "../SingleLineTextBox";
 type Props = {
   question: string;
   components: React.ReactNode[];
+  active?: boolean;
 };
 
-const QuoteQuestion = (props: Props) => {
+const QuoteQuestion = ({ active = false, ...props }: Props) => {
   return (
-    <div className="flex flex-row justify-center items-center h-screen">
-      <div className="flex flex-col items-center bg-white h-4/5 w-2/5 rounded-md shadow-lg py-2 px-4">
-        <span className="w-full">
-          <p className="font-bold text-5xl grad-text pb-6">Question 1</p>
-        </span>
+    <div
+      className={`flex flex-col items-center bg-white h-4/5 rounded-md shadow-lg py-2 px-4 ${
+        !active && "-translate-x-8"
+      }`}
+    >
+      <span className="w-full">
+        <p className="font-bold text-5xl grad-text pb-6">Question 1</p>
+      </span>
 
-        {/* Question Text */}
-        <span className="w-full">
-          <p className="font-semibold text-lg pb-6">{props.question}</p>
-        </span>
+      {/* Question Text */}
+      <span className="w-full">
+        <p className="font-semibold text-lg pb-6">{props.question}</p>
+      </span>
 
-        {/* Loop over components */}
-        {props?.components?.map((Item, idx) => (
-          <div className="w-full py-2">{Item}</div>
-        ))}
+      {/* Loop over components */}
+      {props?.components?.map((item, idx) => (
+        <div className="w-full py-2">{item}</div>
+      ))}
 
-        <div className="grow w-full flex justify-center items-end">
-          <LargeButton text="Next" />
-        </div>
+      <div className="grow w-full flex justify-center items-end">
+        <LargeButton text="Next" />
       </div>
     </div>
   );
