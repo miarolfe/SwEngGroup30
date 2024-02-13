590533bc9e247018aa7ffc4acd5560021bab6515
justin-zngly <justinike.cunningham@outlook.com>
2024-02-13 13:44:37 +0000

    Add initial login skeleton

--- front/life-insurance-front/app/login/page.tsx
+++ front/life-insurance-front/app/login/page.tsx
@@ -1,5 +1,11 @@
 // import Navbar from "../components/Navbar/Navbar";
 
-export default function Login() {
-  return <div>Hello</div>;
-}
+const Login = () => {
+  return (
+    <div className="flex flex-row justify-center items-center h-screen">
+      <div className="bg-white h-4/5 w-2/5 rounded-md shadow-md">100</div>
+    </div>
+  );
+};
+
+export default Login;
