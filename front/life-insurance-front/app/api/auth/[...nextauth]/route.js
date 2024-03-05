b9e5aabe37d730ac1c987d30d85e42ee9a244938
Jake McKenna <jamckenn@tcd.ie>
2024-03-05 16:32:46 +0000

    quotes page now requires valid credentials

--- front/life-insurance-front/app/api/auth/[...nextauth]/route.js
+++ front/life-insurance-front/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
 
 const authOptions = {
     providers: [
@@ -7,8 +10,25 @@ const authOptions = {
             name: "credentials",
             credentials: {},
             async authorize(credentials) {
-                const user = {id: "1"};
-                return user;
+                const {email, password} = credentials;
+                
+                try {
+                    await connectMongoDB();
+                    const user = await User.findOne({email});
+                    if (!user) {
+                        return null;
+                    }
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (!passwordsMatch) {
+                        return null;
+                    }
+
+                    return user;
+                } catch (error) {
+                    console.log(error);
+                }
             },
         })
     ],
