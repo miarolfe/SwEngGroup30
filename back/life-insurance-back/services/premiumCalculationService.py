0634b36ad451b7293e21e674c0339bbeee7b9b4f
Nuoxi Zhang <nzhang@tcd.ie>
2024-03-14 19:01:28 +0000

    premium calculator implemented

---
+++ back/life-insurance-back/services/premiumCalculationService.py
@@ -0,0 +1,65 @@
+import riskCalculationService
+
+
+ENTRY_LEVEL_INSURED_AMOUNT = 300000
+HIGH_LEVEL_INSURED_AMOUNT = 500000
+PREMIUM_LEVEL_INSURED_AMOUNT = 700000
+LEVEL_BASE = 100000
+
+NUMBER_OF_YEAR_INSURED = 50
+
+def calculatePremium(user : dict) -> dict:
+    riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
+    entryLevelCost = calculateEntryLevelPremium(riskScore)
+    highLevelCost = calculateHighLevelPremium(riskScore)
+    premiumLevelCost = calculatePremiumLevelPremium(riskScore)
+
+    entryLevelRecommendation : dict = {
+        "premium": entryLevelCost,
+        "amountInsured" : ENTRY_LEVEL_INSURED_AMOUNT,
+        "yearInsured" : NUMBER_OF_YEAR_INSURED,
+        "comment" : "This is an example of comment message"
+    }
+    highLevelRecommendation : dict = {
+        "premium": highLevelCost,
+        "amountInsured" : HIGH_LEVEL_INSURED_AMOUNT,
+        "yearInsured" : NUMBER_OF_YEAR_INSURED,
+        "comment" : "This is an example of comment message"
+    }
+    premiumLevelRecommendation : dict = {
+        "premium": premiumLevelCost,
+        "amountInsured" : PREMIUM_LEVEL_INSURED_AMOUNT,
+        "yearInsured" : NUMBER_OF_YEAR_INSURED,
+        "comment" : "This is an example of comment message"
+    }
+    return {
+        "entryLevelRecommendation" : entryLevelRecommendation,
+        "highLevelRecommendation" : highLevelRecommendation,
+        "premiumLevelRecommendation" : premiumLevelRecommendation
+    }
+
+
+def calculateEntryLevelPremium(riskScore : float) -> float:
+    # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
+    tolerance : float = LEVEL_BASE * riskScore
+    totalAmount : float = tolerance + ENTRY_LEVEL_INSURED_AMOUNT
+    yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
+    monthlyPremium : float = yearlyPremimun / 12
+    return monthlyPremium/10
+
+def calculateHighLevelPremium(riskScore : float) -> float:
+    # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
+    tolerance : float = LEVEL_BASE * riskScore
+    totalAmount : float = tolerance + HIGH_LEVEL_INSURED_AMOUNT
+    yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
+    monthlyPremium : float = yearlyPremimun / 12
+    return monthlyPremium/10
+
+
+def calculatePremiumLevelPremium(riskScore : float) -> float:
+    # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
+    tolerance : float = LEVEL_BASE * riskScore
+    totalAmount : float = tolerance + PREMIUM_LEVEL_INSURED_AMOUNT
+    yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
+    monthlyPremium : float = yearlyPremimun / 12
+    return monthlyPremium/10
--- back/life-insurance-back/services/riskCalculationService.py
+++ back/life-insurance-back/services/riskCalculationService.py
@@ -4,6 +4,10 @@ from config.database import collection_name as user_collection, collection_disea
 from schemas.users_schema import users_serialiser, user_serialiser
 from bson import ObjectId
 BASE_RISK_SCORE = 10
+SMOKING_RISK_PROPORTION = 0.15
+DRINKING_RISK_PROPORTION = 0.1
+DISEASE_RISK_PROPORTION = 0.6
+EXCERCISE_RISK_PROPORTION = 0.1
 diseaseCursor : pymongo.cursor.Cursor = disease_collection.find()
 diseaseDict : dict = []
 HereditaryRisk : dict = []
@@ -14,8 +18,8 @@ for disease in diseaseCursor:
     HereditaryRisk[diseaseName.casefold()] = disease["HereditaryRisk"]
 
 def getRiskScoreFromUserHealthCondition(user : dict) -> float:
-    riskScoreFromHealthDisease : float =  calculateRiskScorefromHereditaryConditions(user) + calculateRiskScoreFromMedicalHistory(user) + BASE_RISK_SCORE
-    riskScoreFromHabbit : float = getRiskScoreFromSmokingHistory(user) + getRiskScoreFromDrinkingHistory(user) - getDeductionFromHabbit(user)
+    riskScoreFromHealthDisease : float =  ((calculateRiskScorefromHereditaryConditions(user) + calculateRiskScoreFromMedicalHistory(user)) * DISEASE_RISK_PROPORTION) + BASE_RISK_SCORE
+    riskScoreFromHabbit : float = (getRiskScoreFromSmokingHistory(user) * SMOKING_RISK_PROPORTION) + (getRiskScoreFromDrinkingHistory(user) * DRINKING_RISK_PROPORTION) - (getDeductionFromHabbit(user) * EXCERCISE_RISK_PROPORTION)
     totalRiskScore = riskScoreFromHealthDisease + riskScoreFromHabbit
     return totalRiskScore
 
@@ -99,52 +103,52 @@ def calculateRiskScoreFromMedicalHistory(user : dict) -> int:
     return riskScore
 
 
-def get_calculation(user : dict):
-    # user = user_collection.find_one({"_id": ObjectId(id)})
-    diseaseList = disease_collection.find()
+# def get_calculation(user : dict):
+#     # user = user_collection.find_one({"_id": ObjectId(id)})
+#     diseaseList = disease_collection.find()
     
-    #find hereditary and health conditions and puts them into one unique list
-    combined_conditions = user['hereditaryConditions'] + user['healthConditions']
-    unique_combined_conditions = list(set(combined_conditions))
+#     #find hereditary and health conditions and puts them into one unique list
+#     combined_conditions = user['hereditaryConditions'] + user['healthConditions']
+#     unique_combined_conditions = list(set(combined_conditions))
     
-    #check names from lists and use score to calculate insurance
-    baseCost = 1000
-    insuranceCost = 0
-    for disease in diseaseList:
-        diseaseName = disease['Name']
-        if diseaseName.casefold() in (name.casefold() for name in unique_combined_conditions):
-            diseaseScore = disease['Score']
-            insuranceCost = calculate_insurance_cost(diseaseScore, baseCost)
-            if insuranceCost == 666:
-                print("Disease: {} has an incorrect score. please change!!!".format(diseaseName))
-                return {"status": "422 Unprocessable Entity"}
-            else:
-                baseCost = insuranceCost
+#     #check names from lists and use score to calculate insurance
+#     baseCost = 1000
+#     insuranceCost = 0
+#     for disease in diseaseList:
+#         diseaseName = disease['Name']
+#         if diseaseName.casefold() in (name.casefold() for name in unique_combined_conditions):
+#             diseaseScore = disease['Score']
+#             insuranceCost = calculate_insurance_cost(diseaseScore, baseCost)
+#             if insuranceCost == 666:
+#                 print("Disease: {} has an incorrect score. please change!!!".format(diseaseName))
+#                 return {"status": "422 Unprocessable Entity"}
+#             else:
+#                 baseCost = insuranceCost
     
-    if insuranceCost < 3000:
-        results = "Your total health insurance is €{:0.2f} per year".format(insuranceCost)
-        return results
-    else:
-        results = "Unfortunatly we are to provide coverage through our company as your estimated cost is too high"
-        return results
+#     if insuranceCost < 3000:
+#         results = "Your total health insurance is €{:0.2f} per year".format(insuranceCost)
+#         return results
+#     else:
+#         results = "Unfortunatly we are to provide coverage through our company as your estimated cost is too high"
+#         return results
 
-def calculate_insurance_cost(score, cost):
-    errorCode = 666
-    score = int(score)
-    #define the additional costs or multipliers for each score
-    additional_costs = {
-        1: 0.00,  # 0% increase
-        2: 0.10,  # 10% increase
-        3: 0.20,  # 20% increase
-        4: 0.50,  # 50% increase
-        5: 1.00,  # 100% increase (doubles the cost)
-    }
-    #calculate the cost
-    if score in additional_costs:
-        additional_cost = additional_costs[score]
-        total_cost = cost * (1 + additional_cost)
-    else:
-        print("Invalid score. Score must be between 1 and 5. Check database")
-        return errorCode
+# def calculate_insurance_cost(score, cost):
+#     errorCode = 666
+#     score = int(score)
+#     #define the additional costs or multipliers for each score
+#     additional_costs = {
+#         1: 0.00,  # 0% increase
+#         2: 0.10,  # 10% increase
+#         3: 0.20,  # 20% increase
+#         4: 0.50,  # 50% increase
+#         5: 1.00,  # 100% increase (doubles the cost)
+#     }
+#     #calculate the cost
+#     if score in additional_costs:
+#         additional_cost = additional_costs[score]
+#         total_cost = cost * (1 + additional_cost)
+#     else:
+#         print("Invalid score. Score must be between 1 and 5. Check database")
+#         return errorCode
 
-    return total_cost
+#     return total_cost
