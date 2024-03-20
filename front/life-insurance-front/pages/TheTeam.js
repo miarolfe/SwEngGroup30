f6ac506f7462c76c53d98ee2ee3996682acadd9a
luke-boylan <boylanlu@tcd.ie>
2024-03-20 01:38:27 +0000

    team page update

--- front/life-insurance-front/pages/TheTeam.js
+++ front/life-insurance-front/pages/TheTeam.js
@@ -27,114 +27,116 @@ const TheTeam = () => {
                         here you'll find the development team who worked 
                         tirelessly over the last 12 weeks in creating this site
                     </p>
-                    <section alt="front end profiles" className=" items-left text-left">
-                        <br></br>
-                        <h1 className="text-3xl font-mono "> Front-End Team</h1>
-
-                        <div className="flex ">
-                            <img src="/empty-profile.jpg" alt="karolina" width={150} height={150} className="mx-5 mb-5"/>
-                            <div>
-                                <p>Karolina Raczynska - Team Lead</p>
-                                <br />
-                                <p>She does things</p>
-                            </div>
-                        </div>
-
-                        <div className="flex ">
-                        <img src="/empty-profile.jpg" alt="Hamza" width={150} height={150} className="mx-5 mb-5"></img>
-                            <div>
-                                <p>Hamza Bana - Developer</p>
-                                <br />
-                                <p>He does things</p>
-                            </div>
-                        </div>
-
-                        <div className="flex ">
-                        <img src="/empty-profile.jpg" alt="Justin" width={150} height={150} className="mx-5 mb-5"></img>
-                            <div>
-                            <p>Justin Ikechukwu Cunningham - Developer</p>
-                                <br />
-                                <p>He does things</p>
-                            </div>
-                        </div>
-
-                        <div className="flex ">
-                        <img src="/empty-profile.jpg" alt="Ben" width={150} height={150} className="mx-5 mb-5"></img>
-                            <div>
-                            <p>Ben Byrne - Developer</p>
-                                <br />
-                                <p>He does things</p>
-                            </div>
-                        </div>
-
-                        <div className="flex ">
-                        <img src="/empty-profile.jpg" alt="Luke" width={150} height={150} className="mx-5 mb-5"></img>
-                            <div>
-                            <p>Luke Boylan - Developer</p>
-                                <br />
-                                <p>He does things</p>
-                            </div>
-                        </div>
-                    </section>
-
-                        <section alt="backend profiles" className="text-left ">
-                            <h1 className =" text-3xl font-mono"> Back-end Team</h1>
+                    <div className = "flex-row">
+                        <section alt="front end profiles" className="flex-column inline-block float-left text-left bg-gray-100 p-8 rounded-lg shadow-md">
                             <br></br>
+                            <h1 className="text-3xl font-mono text-right"> Front-End Team</h1>
 
                             <div className="flex ">
-                                <img src="/empty-profile.jpg" alt="Nouxi" width={150} height={150} className="mx-5 mb-5" />
+                                <img src="/empty-profile.jpg" alt="karolina" width={150} height={150} className="mx-5 mb-5"/>
                                 <div>
-                                    <p>Nouxi Zhang - Team Lead</p>
-                                    <br />
-                                    <p>He does things</p>
-                                </div>
-                            </div>
-
-                            <div className="flex ">
-                                <img src="/empty-profile.jpg" alt="Jake" width={150} height={150} className="mx-5 mb-5" />
-                                <div>
-                                    <p>Jake McKenna - developer</p>
-                                    <br />
-                                    <p>He does things</p>
-                                </div>
-                            </div>
-
-                            <div className="flex ">
-                            <img src="/empty-profile.jpg" alt="Mia" width={150} height={150} className="mx-5 mb-5"></img>
-                                <div>
-                                <p>Mia Rolfe - Developer</p>
+                                    <p>Karolina Raczynska - Team Lead</p>
                                     <br />
                                     <p>She does things</p>
                                 </div>
                             </div>
 
                             <div className="flex ">
-                            <img src="/empty-profile.jpg" alt="Reece" width={150} height={150} className="mx-5 mb-5"></img>
+                            <img src="/headshots/Hamza.jpg" alt="Hamza" width={150} height={150} className="mx-5 mb-5"></img>
                                 <div>
-                                <p>Reece Webb - Developer</p>
+                                    <p>Hamza Bana - Developer</p>
                                     <br />
                                     <p>He does things</p>
                                 </div>
                             </div>
 
                             <div className="flex ">
-                            <img src="/empty-profile.jpg" alt="kate" width={150} height={150} className="mx-5 mb-5"></img>
+                            <img src="/empty-profile.jpg" alt="Justin" width={150} height={150} className="mx-5 mb-5"></img>
                                 <div>
-                                <p>Kate Byrne - Developer</p>
+                                <p>Justin Ikechukwu Cunningham - Developer</p>
                                     <br />
-                                    <p>She does things</p>
+                                    <p>He does things</p>
                                 </div>
                             </div>
 
                             <div className="flex ">
-                            <img src="/empty-profile.jpg" alt="Essien" width={150} height={150} className="mx-5 mb-5" />
+                            <img src="/empty-profile.jpg" alt="Ben" width={150} height={150} className="mx-5 mb-5"></img>
                                 <div>
-                                    <p>Essien Thompson - Developer</p>
+                                <p>Ben Byrne - Developer</p>
+                                    <br />
+                                    <p>He does things</p>
+                                </div>
+                            </div>
+
+                            <div className="flex ">
+                            <img src="/empty-profile.jpg" alt="Luke" width={150} height={150} className="mx-5 mb-5"></img>
+                                <div>
+                                <p>Luke Boylan - Developer</p>
                                     <br />
                                     <p>He does things</p>
                                 </div>
                             </div>
                         </section>
+
+                            <section alt="backend profiles" className=" flex-column inline-block float-right text-right justify-right bg-gray-100 p-8 rounded-lg shadow-md">
+                                <h1 className =" text-3xl font-mono text-left"> Back-end Team</h1>
+                                <br></br>
+
+                                <div className="flex ">
+                                    <div>
+                                        <p>Nouxi Zhang - Team Lead</p>
+                                        <br />
+                                        <p>He does things </p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="Nouxi" width={150} height={150} className="float right mx-5 mb-5" />
+                                </div>
+
+                                <div className="flex ">
+                                    <div>
+                                        <p>Jake McKenna - developer</p>
+                                        <br />
+                                        <p>He does things</p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="Jake" width={150} height={150} className="mx-5 mb-5" />
+                                </div>
+
+                                <div className="flex ">
+                                    <div>
+                                    <p>Mia Rolfe - Developer</p>
+                                        <br />
+                                        <p>She does things</p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="Mia" width={150} height={150} className="mx-5 mb-5"></img>
+                                </div>
+
+                                <div className="flex ">
+                                    <div>
+                                    <p>Reece Webb - Developer</p>
+                                        <br />
+                                        <p>He does things</p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="Reece" width={150} height={150} className="mx-5 mb-5"></img>
+                                </div>
+
+                                <div className="flex ">
+                                    <div>
+                                    <p>Kate Byrne - Developer</p>
+                                        <br />
+                                        <p>She does things</p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="kate" width={150} height={150} className="mx-5 mb-5"></img>
+                                </div>
+
+                                <div className="flex ">
+                                    <div>
+                                        <p>Essien Thompson - Developer</p>
+                                        <br />
+                                        <p>He does things</p>
+                                    </div>
+                                    <img src="/empty-profile.jpg" alt="Essien" width={150} height={150} className="mx-5 mb-5" />
+                                </div>
+                            </section>
+                    </div>
 
                 </section>
             </main>
