4e10d1c2fafbe2ddee5e58dce702c3487b8fd20a
justin-zngly <justinike.cunningham@outlook.com>
2024-03-25 17:11:53 +0000

    Change quote layout

--- front/life-insurance-front/components/Quote/Quote.tsx
+++ front/life-insurance-front/components/Quote/Quote.tsx
@@ -1,21 +1,27 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import CountUp from "react-countup";
 
 const QuoteAddonBlock = ({
   name,
   selected,
   onClick,
+  start,
+  end,
 }: {
   name: string;
   selected: boolean;
   onClick: VoidFunction;
+  start?: boolean;
+  end?: boolean;
 }) => {
   return (
     <button
       onClick={onClick}
       className={
-        "w-4/5 h-28 rounded-lg glass transition hover:glass-darker p-2" +
-        (selected ? " glass-darker" : "")
+        "w-60 grow glass-darker-noshadow transition hover:glass-noshadow p-2" +
+        (selected ? " glass-noshadow" : "") +
+        (start ? " border-tl-md" : "") +
+        (end ? " border-bl-md" : "")
       }
     >
       <p className="text-3xl text-white font-bold">{name}</p>
@@ -67,40 +73,101 @@ const Quote = () => {
     setCurrentQuote(placeholderData[selected]);
   }, [selected]);
 
+  const translateBy = useMemo(() => {
+    if (selected === "highLevelRecommendation") return "1.5";
+    if (selected === "entryLevelRecommendation") return "3";
+    return 0;
+  }, [selected]);
+
   return (
-    <div className="h-full flex flex-row">
-      <div className="flex flex-col w-1/2 justify-around items-center">
-        <QuoteAddonBlock
-          name="Premium"
-          selected={selected === "premiumLevelRecommendation"}
-          onClick={() => setSelected("premiumLevelRecommendation")}
-        />
-        <QuoteAddonBlock
-          name="High"
-          selected={selected === "highLevelRecommendation"}
-          onClick={() => setSelected("highLevelRecommendation")}
-        />
-        <QuoteAddonBlock
-          name="Entry"
-          selected={selected === "entryLevelRecommendation"}
-          onClick={() => setSelected("entryLevelRecommendation")}
-        />
-      </div>
-      <div className="flex justify-center items-center grow text-8xl font-bold text-white">
-        <CountUp
-          start={countFrom}
-          end={currentQuote["premium"]}
-          duration={0.65}
-          separator=""
-          decimal="."
-          prefix="€"
-        >
-          {({ countUpRef }) => (
-            <div>
-              <span ref={countUpRef} />
+    <div className="h-full flex flex-col justify-center items-center">
+      <div className="w-5/6 h-5/6 flex rounded-md overflow-hidden">
+        <div className="flex flex-col justify-around items-center">
+          <QuoteAddonBlock
+            name="Premium"
+            start
+            selected={selected === "premiumLevelRecommendation"}
+            onClick={() => setSelected("premiumLevelRecommendation")}
+          />
+          <QuoteAddonBlock
+            name="High"
+            selected={selected === "highLevelRecommendation"}
+            onClick={() => setSelected("highLevelRecommendation")}
+          />
+          <QuoteAddonBlock
+            name="Entry"
+            end
+            selected={selected === "entryLevelRecommendation"}
+            onClick={() => setSelected("entryLevelRecommendation")}
+          />
+        </div>
+        {/* Quote on right */}
+        <div className="grow flex flex-col text-6xl glass-noshadow text-white px-4 py-2">
+          {/* <div className="w-5/6 h-5/6 flex flex-col justify-center items-center text-6xl text-white rounded-md"> */}
+          <div
+            className="flex text-5xl"
+            style={{
+              lineHeight: "1.5em",
+              height: "1.5em",
+              overflow: "hidden",
+            }}
+          >
+            <p className="mr-6 font-bold">Recommendation</p>
+            <div
+              className="flex flex-col text-white"
+              style={{
+                transition: "0.5s all ease-in-out",
+                transform: "translateY(-" + translateBy + "em)",
+              }}
+            >
+              <p>Premium</p>
+              <p>High</p>
+              <p>Entry</p>
             </div>
-          )}
-        </CountUp>
+          </div>
+          <CountUp
+            start={countFrom}
+            end={currentQuote.premium}
+            duration={0.65}
+            separator=""
+            decimal="."
+            prefix="€"
+          >
+            {({ countUpRef }) => (
+              <div className="mb-4">
+                <span ref={countUpRef} />
+              </div>
+            )}
+          </CountUp>
+          {/* Details table */}
+          <table className="text-xl border-white overflow-hidden border-separate border-spacing-0">
+            <tr className="">
+              <td className="rounded-tl-md border-l-2 border-t-2 border-b-2 pl-2">
+                Annual cost
+              </td>
+              <td className="rounded-tr-md border-t-2 border-r-2 border-b-2">
+                €3000
+              </td>
+            </tr>
+            <tr>
+              <td className="border-l-2 border-b-2 pl-2">
+                Maximum insured amount
+              </td>
+              <td className="border-r-2 border-b-2">
+                €{currentQuote.amountInsured}
+              </td>
+            </tr>
+            <tr>
+              <td className="rounded-bl-md border-l-2 border-b-2 pl-2">
+                Maximum insured years
+              </td>
+              <td className="rounded-br-md border-r-2 border-b-2">
+                {currentQuote.maxYearInsured} years
+              </td>
+            </tr>
+          </table>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
