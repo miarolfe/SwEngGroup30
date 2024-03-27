cd1f24e04d664132f751df836aab875d80cec0c9
Mia Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2024-03-27 16:07:24 +0000

    Fix typo

--- back/life-insurance-back/main.py
+++ back/life-insurance-back/main.py
@@ -11,7 +11,6 @@ app.include_router(user_api_router)
 app.include_router(premium_calculator_router)
 
 app.include_router(quoteRequestRouter)
-=======
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust to your needs, "" allows all origins
