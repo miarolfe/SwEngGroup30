158e268fdb16e0a2465078825043a72c4253c803
Mia Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2024-03-27 16:08:28 +0000

    Fix typo

--- back/life-insurance-back/main.py
+++ back/life-insurance-back/main.py
@@ -9,8 +9,8 @@ app = FastAPI()
 
 app.include_router(user_api_router)
 app.include_router(premium_calculator_router)
-
 app.include_router(quoteRequestRouter)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust to your needs, "" allows all origins
