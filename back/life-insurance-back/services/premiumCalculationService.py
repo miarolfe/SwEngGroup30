e72b5706d39a9039b33438ce6a0f2528c12ac1f3
byrnek42 <127962845+byrnek42@users.noreply.github.com>
2024-03-27 22:20:57 +0000

    fine-tune-calculator

--- back/life-insurance-back/services/premiumCalculationService.py
+++ back/life-insurance-back/services/premiumCalculationService.py
@@ -7,30 +7,42 @@ LEVEL_BASE = 100000
 
 NUMBER_OF_YEAR_INSURED = 50
 
+INCREASE_PER_PERCENTAGE = 4500
+
 def calculatePremium(user : dict) -> dict:
     riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
+
+    if (riskScore/100) > 85:
+        return{
+            "message" : "Sorry, we are unable to calculate premium for you. Please consult with our underwriters"
+        }
+    
     entryLevelCost = calculateEntryLevelPremium(riskScore)
     highLevelCost = calculateHighLevelPremium(riskScore)
     premiumLevelCost = calculatePremiumLevelPremium(riskScore)
     NUMBER_OF_YEAR_INSURED = riskCalculationService.getYearsInsuredLeft(user)
+
+    entryLevelComment = "This is a standard recommendation suitable for most users."
+    highLevelComment = "This recommendation offers higher coverage for those seeking additional security."
+    premiumLevelComment = "This premium recommendation is for users prioritising extensive coverage."
     
     entryLevelRecommendation : dict = {
         "premium": entryLevelCost,
         "amountInsured" : ENTRY_LEVEL_INSURED_AMOUNT,
         "maxYearInsured" : NUMBER_OF_YEAR_INSURED,
-        "comment" : "This is an example of comment message"
+        "comment" : entryLevelComment
     }
     highLevelRecommendation : dict = {
         "premium": highLevelCost,
         "amountInsured" : HIGH_LEVEL_INSURED_AMOUNT,
         "maxYearInsured" : NUMBER_OF_YEAR_INSURED,
-        "comment" : "This is an example of comment message"
+        "comment" : highLevelComment
     }
     premiumLevelRecommendation : dict = {
         "premium": premiumLevelCost,
         "amountInsured" : PREMIUM_LEVEL_INSURED_AMOUNT,
         "maxYearInsured" : NUMBER_OF_YEAR_INSURED,
-        "comment" : "This is an example of comment message"
+        "comment" : premiumLevelComment
     }
     return {
         "entryLevelRecommendation" : entryLevelRecommendation,
@@ -41,25 +53,31 @@ def calculatePremium(user : dict) -> dict:
 
 def calculateEntryLevelPremium(riskScore : float) -> float:
     # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
-    tolerance : float = LEVEL_BASE * riskScore
+    riskPercentage : float = riskScore/100
+    tolerance : float = (riskPercentage/5) * INCREASE_PER_PERCENTAGE
     totalAmount : float = tolerance + ENTRY_LEVEL_INSURED_AMOUNT
     yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
     monthlyPremium : float = yearlyPremimun / 12
-    return monthlyPremium/100
+    return monthlyPremium
+
+    
 
 def calculateHighLevelPremium(riskScore : float) -> float:
     # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
-    tolerance : float = LEVEL_BASE * riskScore
+    riskPercentage : float = riskScore/100
+    tolerance : float = (riskPercentage/5) * INCREASE_PER_PERCENTAGE
     totalAmount : float = tolerance + HIGH_LEVEL_INSURED_AMOUNT
     yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
     monthlyPremium : float = yearlyPremimun / 12
-    return monthlyPremium/100
+    return monthlyPremium
 
 
 def calculatePremiumLevelPremium(riskScore : float) -> float:
     # riskScore : float = riskCalculationService.getRiskScoreFromUserHealthCondition(user)
-    tolerance : float = LEVEL_BASE * riskScore
+    riskPercentage : float = riskScore/100
+    tolerance : float = (riskPercentage/5) * INCREASE_PER_PERCENTAGE
     totalAmount : float = tolerance + PREMIUM_LEVEL_INSURED_AMOUNT
     yearlyPremimun : float = totalAmount/NUMBER_OF_YEAR_INSURED
     monthlyPremium : float = yearlyPremimun / 12
-    return monthlyPremium/100
+    return monthlyPremium
+
