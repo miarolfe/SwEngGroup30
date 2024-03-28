afd5139c04e94c02ee5739963d8b44ad1803d68a
justin-zngly <justinike.cunningham@outlook.com>
2024-03-28 20:45:38 +0000

    Add open/close states

--- front/life-insurance-front/components/Chatbot/Chatbot.tsx
+++ front/life-insurance-front/components/Chatbot/Chatbot.tsx
@@ -1,5 +1,10 @@
-import { XMarkIcon, PaperAirplaneIcon } from "@heroicons/react/20/solid";
-import { useMemo } from "react";
+"use client";
+import { useMemo, useState } from "react";
+import {
+  XMarkIcon,
+  PaperAirplaneIcon,
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/20/solid";
 
 export type Message = {
   message: String;
@@ -22,11 +27,26 @@ const ChatbotMessage = ({ message }: { message: Message }) => {
 };
 
 const Chatbot = ({ messages }: { messages: Message[] }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  if (!open)
+    return (
+      <button
+        onClick={() => setOpen(true)}
+        className="flex transition-all justify-center items-center glass h-20 w-20 rounded-full fixed bottom-6 right-6 hover:cursor-pointer hover:glass-darker"
+      >
+        <ChatBubbleBottomCenterTextIcon className="w-6 text-white" />
+      </button>
+    );
+
   return (
     <div className="flex flex-col glass w-1/3 h-[36rem] fixed bottom-6 right-6 rounded-md overflow-hidden">
       <div className="flex px-2 justify-between items-center w-full h-10 glass">
         <p className="text-slate-800">Chatbot</p>
-        <XMarkIcon className="w-6 h-full transition-all text-slate-800 hover:text-white hover:cursor-pointer" />
+        <XMarkIcon
+          onClick={() => setOpen(false)}
+          className="w-6 h-full transition-all text-slate-800 hover:text-white hover:cursor-pointer"
+        />
       </div>
       <div className="overflow-scroll overflow-hidden p-2 grow">
         {messages.map((item, i) => {
@@ -34,7 +54,7 @@ const Chatbot = ({ messages }: { messages: Message[] }) => {
         })}
       </div>
       <div className="flex justify-between items-center h-16 w-full border-t-slate-400 border-t-2 px-2">
-        <input className="h-2/3 w-4/5 border border-white rounded-md px-4 bg-transparent"></input>
+        <input className="h-2/3 w-4/5 border border-white rounded-md px-4 bg-transparent outline-none text-white"></input>
         <PaperAirplaneIcon className="w-8 text-white transition-all hover:text-slate-300 hover:cursor-pointer" />
       </div>
     </div>
