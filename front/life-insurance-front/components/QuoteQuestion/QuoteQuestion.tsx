d46ff5f570c90d1122d9d0d01f93a33a9423f02d
justin-zngly <justinike.cunningham@outlook.com>
2024-03-28 08:50:19 +0000

    Small styling + add key to element

--- front/life-insurance-front/components/QuoteQuestion/QuoteQuestion.tsx
+++ front/life-insurance-front/components/QuoteQuestion/QuoteQuestion.tsx
@@ -100,15 +100,10 @@ const QuoteQuestion = ({ active = false, ...props }: Props) => {
       </span>
 
       <div className="w-full h-full flex flex-col px-4">
-        {/* Question Text */}
-        {/* <span className="w-full">
-          <p className="font-medium text-white text-2xl pb-4">
-            {props.question}
-          </p>
-        </span> */}
-
         {props?.newComps.map((item, idx) => (
-          <div className="w-full py-2">{getComponent(item, idx)}</div>
+          <div key={idx} className="w-full py-2">
+            {getComponent(item, idx)}
+          </div>
         ))}
 
         <div className="grow w-full flex justify-around items-end">
