d5ec22057a8b99052823f4943f3e19aa9d90afd5
justin-zngly <justinike.cunningham@outlook.com>
2024-03-28 20:31:43 +0000

    Add message bubbles

--- front/life-insurance-front/app/underwriter/page.tsx
+++ front/life-insurance-front/app/underwriter/page.tsx
@@ -1,10 +1,21 @@
-import Chatbot from "@/components/Chatbot/Chatbot";
+import Chatbot, { type Message } from "@/components/Chatbot/Chatbot";
+
+const sample: Message[] = [
+  {
+    message: "Hello Admin! What can I help you with today?",
+    from: "chatbot",
+  },
+  {
+    message: "How are you?",
+    from: "you",
+  },
+];
 
 const UnderwriterPage = () => {
   return (
     <div className="main-bg min-h-screen h-full w-full p-2">
       <div>Underwriter page</div>
-      <Chatbot />
+      <Chatbot messages={sample} />
     </div>
   );
 };
--- front/life-insurance-front/components/Chatbot/Chatbot.tsx
+++ front/life-insurance-front/components/Chatbot/Chatbot.tsx
@@ -1,14 +1,40 @@
 import { XMarkIcon, PaperAirplaneIcon } from "@heroicons/react/20/solid";
+import { useMemo } from "react";
 
-const Chatbot = () => {
+export type Message = {
+  message: String;
+  from: "you" | "chatbot";
+};
+
+const ChatbotMessage = ({ message }: { message: Message }) => {
+  const style = useMemo(() => {
+    if (message.from === "chatbot") return "bg-white rounded-bl-none";
+    return "bg-violet-700 ml-auto text-white rounded-br-none";
+  }, [message]);
+
   return (
-    <div className="flex glass w-1/3 h-[36rem] fixed bottom-6 right-6 rounded-md overflow-hidden">
-      <div className="flex px-2 justify-between items-center absolute top-0 w-full h-10 glass">
+    <div
+      className={`text-sm min-h-6 w-1/2 my-2 p-2 shadow rounded-md ${style}`}
+    >
+      {message.message}
+    </div>
+  );
+};
+
+const Chatbot = ({ messages }: { messages: Message[] }) => {
+  return (
+    <div className="flex flex-col glass w-1/3 h-[36rem] fixed bottom-6 right-6 rounded-md overflow-hidden">
+      <div className="flex px-2 justify-between items-center w-full h-10 glass">
         <p className="text-slate-800">Chatbot</p>
         <XMarkIcon className="w-6 h-full transition-all text-slate-800 hover:text-white hover:cursor-pointer" />
       </div>
-      <div className="flex justify-between items-center absolute bottom-0 h-16 w-full border-t-slate-400 border-t-2 px-2">
-        <input className="h-2/3 w-4/5 border border-white rounded-full px-4 bg-transparent"></input>
+      <div className="overflow-scroll overflow-hidden p-2 grow">
+        {messages.map((item, i) => {
+          return <ChatbotMessage message={item} />;
+        })}
+      </div>
+      <div className="flex justify-between items-center h-16 w-full border-t-slate-400 border-t-2 px-2">
+        <input className="h-2/3 w-4/5 border border-white rounded-md px-4 bg-transparent"></input>
         <PaperAirplaneIcon className="w-8 text-white transition-all hover:text-slate-300 hover:cursor-pointer" />
       </div>
     </div>
