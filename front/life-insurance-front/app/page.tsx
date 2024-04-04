31b0978cc5f2de29036d41403094caacb1841b0c
raczynsk <raczynsk@tcd.ie>
2024-04-04 20:54:51 +0100

    cognito configuration login

--- front/life-insurance-front/app/page.tsx
+++ front/life-insurance-front/app/page.tsx
@@ -1,17 +1,20 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import { Amplify } from 'aws-amplify';
+import awsconfig from "../aws-exports";
 //import config from '../aws-exports';
 
-Amplify.configure({
-    Auth: {
-        Cognito: {
-            userPoolClientId: '557612919820',
-            userPoolId: 'eu-west-2_K0AH1uos7'
+// Amplify.configure({
+//     Auth: {
+//         Cognito: {
+//             userPoolClientId: '557612919820',
+//             userPoolId: 'eu-west-2_K0AH1uos7',
+//             identityPoolId: 'eu-west-2:887b10c4-d131-41b2-83bd-9bfa0f17c4c6'
+//         }
+//     }
+// });
+Amplify.configure(awsconfig);
 
-        }
-    }
-});
 
 const Index: React.FC = () => {
   return (
