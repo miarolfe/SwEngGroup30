75691f80caf3de1bf05f7159a3528e63c6d67223
JustCunn <justinike.cunningham@outlook.com>
2024-04-07 21:39:37 +0100

    Change footer text positioning

--- front/life-insurance-front/components/Footer.tsx
+++ front/life-insurance-front/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex items-center w-full h-32 glass-slate text-white">
+    <div className="flex items-center w-full h-32 glass-slate text-white px-4">
       <div className="h-full w-fit flex items-center justify-around p-2">
         <a
           className="h-full"
@@ -29,7 +29,7 @@ const Footer: React.FC = () => {
           />
         </a>
       </div>
-      <div className="content-center grow">
+      <div className="flex justify-end grow">
         <p>© SwEng Group 30, 2024. All rights reserved.</p>
       </div>
     </div>
