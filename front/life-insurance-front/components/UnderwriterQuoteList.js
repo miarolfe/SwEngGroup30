1def46dec5f351b98b17c8e069984f637d1ed53c
Jake McKenna <jamckenn@tcd.ie>
2024-04-08 23:59:46 +0100

    Began work one requests

--- front/life-insurance-front/components/UnderwriterQuoteList.js
+++ front/life-insurance-front/components/UnderwriterQuoteList.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 import Modal from "@/components/Modal/Modal";
 import UnderwriterQuoteCard from "./UnderwriterQuoteCard";
+import { useSession } from "next-auth/react";
 const UnderwriterQuoteList = () => {
     const [quoteData, setQuoteData] = useState([]);
+    const {data: session} = useSession();
 
     const initSetup = async () => {
-        await fetch("http://0.0.0.0:8000/api/quoteRequest/get").then((data) => data.json()).then((data) => setQuoteData(data));
+        await fetch("http://0.0.0.0:8000/api/quoteRequest/get", {method: "GET", headers: {"authID": session?.user?.id}})
+        .then((data) => data.json())
+        .then((data) => setQuoteData(data));
     }
     
     useEffect(() => {
@@ -14,8 +18,7 @@ const UnderwriterQuoteList = () => {
 
     return (
         <div>
-            {quoteData.map(quote => <Modal triggerEl={<button>{quote.medicalRecord.patientName}</button>}><UnderwriterQuoteCard quote={quote}/></Modal>)}
-            <p> test the inputted text is recognised</p>
+            {quoteData.map(quote => <Modal triggerEl={<div className="glass border-[1px] border-white mb-2 table-auto cursor-pointer">{quote.medicalRecord.patientName}</div>}><UnderwriterQuoteCard quote={quote}/></Modal>)}
         </div>
     );
 }
