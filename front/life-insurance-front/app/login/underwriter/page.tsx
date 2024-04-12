98a908c8023629bd547321a84976a7ef39cbc303
Jake McKenna <jamckenn@tcd.ie>
2024-04-12 13:50:10 +0100

    Fixed compile issues

--- front/life-insurance-front/app/login/underwriter/page.tsx
+++ front/life-insurance-front/app/login/underwriter/page.tsx
@@ -1,77 +1,108 @@
-import '@/app/globals.css';
-import * as React from 'react';
+'use client'
+import '../../../app/globals.css';
+import React, { useState } from 'react';
 import Image from "next/image";
-import styles from '@/styles/mainApp.module.css';
-import { Auth } from 'aws-amplify';
+import styles from '../../../styles/mainApp.module.css';
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
-const LoginUnderwriter = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [response, setResponse] = React.useState('');
-  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
-    try {
-      const user = await Auth.signIn(username, password);
-      console.log(user); // User is signed in
-      const response = await fetch(`/api/quoteRequest/get`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
+const LoginPage = () => {
+    const router = useRouter();
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+  
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+  
+      try {
+        const res = await signIn("credentials", {
+          email: username,
+          password,
+          redirect: false,
+        });
+  
+        if (!res || res.error) {
+          console.log("Invalid Crentials");
+          return;
         }
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        setResponse(JSON.stringify(data));
-        // Handle success
-      } else {
-        // Handle error
-        setResponse('request failed' + response.status);
+  
+        await router.push("/underwriter");
+        console.log("hurray");
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.error('Login failed', error);
-    }
-  };
+    };
+  
+    const handleRegister = async (
+      e: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+      try {
+        const resUserExists = await fetch("/api/userExists", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email: username }),
+        });
+  
+        const { user } = await resUserExists.json();
+        if (user) {
+          console.log("User Exists");
+          return;
+        }
+  
+        const res = await fetch("/api/register", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: username,
+            password,
+          }),
+        });
+  
+        if (res.ok) {
+          console.log("Worked");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
 
-  return (
-      <div className="min-h-screen flex flex-col justify-center items-center">
-        <form onSubmit={handleLogin} className="w-80">
-          <div className="mb-4">
-            <div className={styles.loginLogo}>
-              <Image src="/logo.jpeg" alt="Munich RE" width={150} height={100}/>
-            </div>
-            <label htmlFor="username" className="block mb-2">Username:</label>
-            <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-2">Password:</label>
-            <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            />
-          </div>
-          <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">Login</button>
-        </form>
-        {response && (
-            <div className="mt-4">
-              <p>Response from server:</p>
-              <div className="p-4 bg-gray-200 rounded">{response}</div>
-            </div>
-        )}
-      </div>
-  );
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center">
+            <form onSubmit={handleLogin} className="w-80">
+                <div className="mb-4">
+                    <div className={styles.loginLogo}>
+                        <Image src="/logo.png" alt="Munich RE" width={150} height={100}/>
+                    </div>
+                    <label htmlFor="username" className="block mb-2">Email:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="password" className="block mb-2">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+                <button type="submit" className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors">Login</button>
+            </form>
+        </div>
+    );
 
 };
 
-export default LoginUnderwriter;
+export default LoginPage;
